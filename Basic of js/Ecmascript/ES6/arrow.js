b1681d73761bb5dba86d14a8995f3229ffbbee28
juanpablogt <juapabgonzalez@utp.edu.co>
2025-02-22 20:22:18 -0500

    ES6

---
+++ Basic of js/Ecmascript/ES6/arrow.js
@@ -0,0 +1,12 @@
+//las funciones se pueden definir como
+
+ function sum(a, b) {
+
+    return a + b;
+}
+
+const square = (a) => {
+    return a * a;
+}
+
+const square2 = a => a * a;
---
+++ Basic of js/Ecmascript/ES6/datos.js
@@ -0,0 +1,9 @@
+let animal = 'dog';
+animal = 'cat';
+console.log(animal); 
+
+
+//Las constantes no pueden ser reasignadas
+const animals = 'cat';
+animals = 'lion';
+console.log(animals);
