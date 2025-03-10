814cc3622c99febd4e9a977859c10d95aeb9e3cb
juanpablogt <juapabgonzalez@utp.edu.co>
2025-03-10 16:55:31 -0500

    promesas es6

---
+++ Basic of js/Ecmascript/ES6/objet_literals.js
@@ -0,0 +1,5 @@
+function newuser(user, age, country,uId){
+    return {user, age, country, id:uId}
+}
+
+console.log(newuser('oscar', 32, 'MX',1));
---
+++ Basic of js/Ecmascript/ES6/promise.js
@@ -0,0 +1,13 @@
+const anotherFunction = () => {
+    return new Promise((resolve, reject) => {
+        if (false) {
+            resolve('Hey!');
+        } else {
+            reject('Ups!');
+        }
+    });
+}
+
+anotherFunction()
+    .then(response => console.log(response))
+    .catch(error => console.error(error)); // Ups!
