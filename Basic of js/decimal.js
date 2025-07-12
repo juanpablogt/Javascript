ffed7ca73f0db60563e18c4ce65a5615c005519c
juanpablogt <juapabgonzalez@utp.edu.co>
2025-07-12 12:08:20 -0500

    curso basico de js

---
+++ Basic of js/decimal.js
@@ -0,0 +1,10 @@
+let decimal = 0.1 + 0.2;
+console.log(decimal);
+// pone el numero de decimalmales que necesitas
+console.log(decimal.toFixed(1));
+
+//Operaciones avanzadas
+const raiz = Math.sqrt(25);
+const valorAbsoluto = Math.abs(-10);
+const aleatorio = Math.random();
+console.log(raiz, valorAbsoluto, aleatorio);
