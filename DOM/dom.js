696ebbba3943ca037e94c10520219950a714e6e2
juanpablogt <juapabgonzalez@utp.edu.co>
2025-07-20 20:17:49 -0500

    manipulacion de estilos con js

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,2 +1,8 @@
 const title = document.getElementById("welcome");
 title.textContent = "Welcome to My Awesome Website";
+
+title.style.color = "red";
+
+const nav = document.querySelector("#main-nav");
+nav.style.backgroundColor = "lightblue";
+nav.style.fontSize = "40px";
