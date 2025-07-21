3f893d43066a379c44e0ee4d4c7c5602a0753068
juanpablogt <juapabgonzalez@utp.edu.co>
2025-07-20 20:53:04 -0500

    ocultar o mostrar sesion manipulando el DOM con js

--- DOM/dom.js
+++ DOM/dom.js
@@ -6,3 +6,13 @@ title.style.color = "red";
 const nav = document.querySelector("#main-nav");
 nav.style.backgroundColor = "lightblue";
 nav.style.fontSize = "40px";
+
+const button = document.querySelector("#boton");
+
+button.addEventListener("click", () => {
+  nav.classList.toggle("hidden");
+    // Toggle the visibility of the navigation menu
+  button.textContent = nav.classList.contains("hidden") ? "mostrar" : "ocultar";
+    // Change button text based on visibility
+});
+
