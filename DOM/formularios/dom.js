9c920f6c4564fbaad9413824fae9120310014dc3
juanpablogt <juapabgonzalez@utp.edu.co>
2025-07-23 22:17:33 -0500

    mno

--- DOM/formularios/dom.js
+++ DOM/formularios/dom.js
@@ -3,5 +3,4 @@ form.addEventListener('submit', (event) => {
     event.preventDefault(); // Evita el envío del formulario
     const name = form.elements['name'].value;
     console.log(name); // Muestra el valor del campo 'name' en la consola
-    // Aquí puedes agregar más lógica para manejar el formulario
 });
